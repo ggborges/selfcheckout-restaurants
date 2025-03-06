8700a1044fd4fc867fde620d73e01b8ca05343fb
Gustavo Borges <ggborges@cin.ufpe.br>
2025-03-05 21:41:44 -0300

    feat: add chevronIcon and scrollIcon  button

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
@@ -21,18 +23,36 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
   const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  {
+    /* Verifying if restaurant exists */
+  }
   if (!restaurant) {
     return notFound();
   }
+
   return (
     <div>
       <div className="relative h-[250px] w-full">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute left-4 top-4 z-50 rounded-full"
+        >
+          <ChevronLeftIcon />
+        </Button>
         <Image
-          src={restaurant?.coverImageUrl}
+          src={restaurant.coverImageUrl}
           alt={restaurant.name}
           fill
           className="object-cover"
         />
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute right-4 top-4 z-50 rounded-full"
+        >
+          <ScrollTextIcon />
+        </Button>
       </div>
     </div>
   );
